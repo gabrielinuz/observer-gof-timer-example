c5d2a2f54063875f391f46f17334c704f649bf7e
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-05-21 09:26:09 -0300

    refact-01

---
+++ refact-01/AnalogClock.h
@@ -0,0 +1,31 @@
+#ifndef ANALOGCLOCK_H
+#define ANALOGCLOCK_H
+
+#include <iostream>
+#include <string>
+
+#include "Widget.h"
+#include "Observer.h"
+#include "Subject.h"
+#include "ClockTimer.h"
+
+using namespace std;
+/****************************************CONCRETE ANALOG CLOCK***********************************/
+/**An AnalogClock class can be defined in the same way.**/
+class AnalogClock: public Widget, public Observer
+{
+    public:
+        AnalogClock(ClockTimer*);
+        virtual ~AnalogClock();
+
+        // overrides Observer operation
+        virtual void Update(Subject*);
+
+        // overrides Widget operation;
+        // defines how to draw the digital clock
+        virtual void Draw();
+
+    private:
+        ClockTimer* _subject;
+};
+#endif // ANALOGCLOCK_H
