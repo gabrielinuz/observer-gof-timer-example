588b11d4590d9f038e3c492e67ebb2eea50ce818
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-06-04 10:01:31 -0300

    refactoring

--- observer-refactoring-02/main.cpp
+++ observer-refactoring-02/main.cpp
@@ -4,6 +4,12 @@
     Author      : Erich Gamma, Richard Helm, Ralph Johnson and John Vlissides
 **/
 
+#ifdef __unix__
+    #include <unistd.h>
+#elif defined(_WIN32) || defined(WIN32)
+    #include <windows.h>
+#endif // defined
+
 #include <iostream>
 
 #include "include/ClockTimer.h"
@@ -21,9 +27,21 @@ int main()
     new AnalogClock(timer);
     new DigitalClock(timer);
 
-    timer->Tick();
-    timer->Tick();
-    timer->Tick();
+//    timer->Tick();
+//    timer->Tick();
+//    timer->Tick();
+
+    while(1)
+    {
+        timer->Tick();
+        #ifdef __unix__
+            sleep(1);
+            system("clear");
+        #elif defined(_WIN32) || defined(WIN32)
+            Sleep( 1000 );
+            system("cls");
+        #endif // defined
+    }
 
     return 0;
 }
